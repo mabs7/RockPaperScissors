6def35abfbd3bb24ffe765ebd1b410ab0c5ba5bb
mabs7 <29295676+mabs7@users.noreply.github.com>
2024-06-16 23:54:51 +0500

    Added to main logic of the game.

--- app.js
+++ app.js
@@ -1,19 +1,47 @@
+let computerChoice;
+let humanChoice;
 function getComputerChoice () {
   // return Math.floor(Math.random() * 3);
   if (Math.floor(Math.random() *3) === 0 ) {
-    return "Rock";
+    computerChoice = "rock";
   }
   else if (Math.floor(Math.random() * 3) === 1) {
-    return "Paper";
+    computerChoice = "paper";
   }
   else {
-    return "Scissors";
+    computerChoice = "scissors"
   }
 }
 function getHumanChoice () {
-  prompt ("What is your choice Rock, Paper or Scissors?");
-  if (prompt()=== Rock) {
-    return "Rock"
+  humanChoice = prompt ("What is your choice");
+  if (humanChoice.toLowerCase() === "rock") {
+    humanChoice = "rock"
+  }
+  else if (humanChoice.toLowerCase() === "paper") {
+    humanChoice = "paper"
+  }
+  else if (humanChoice.toLowerCase() === "scissors") {
+    humanChoice = "scissors"
+  }
+  else {
+    prompt("enter a proper word ie: Rock, papaer or Scissor:")
   }
 }
-getHumanChoice();
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound () {
+  if ((humanChoice == "rock" && computerChoice == "paper") ||(humanChoice == "paper" && computerChoice =="scissors") || (humanChoice == "scissors" && computerChoice =="rock")){
+    alert ("you loose");
+  }
+  else if ((humanChoice == "scissors" && computerChoice =="paper") || (humanChoice == "rock" && computerChoice =="scissors") || (humanChoice == "paper" && computerChoice =="rock")){
+    alert ("You win");
+  }
+  else 
+   {
+    alert("You and compter choose the same thing!! Chosse again...");
+  }
+}
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+playRound (humanSelection, computerSelection);
